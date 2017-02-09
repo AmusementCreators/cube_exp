c0c74dfb171ea58e0cffba0dea8f29bddaf6d470
wisteria <wisteria0x0f@users.noreply.github.com>
2017-02-09 13:47:45 +0900

    ソリューション追加

---
+++ cube_exp/cube_exp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cube_exp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!asd.Engine.Initialize("cube [experimental]", 1280, 720, new asd.EngineOption()))return;
+
+            asd.Engine.ChangeScene(new Scene.Game());
+
+            while(asd.Engine.DoEvents())
+            {
+                asd.Engine.Update();
+            }
+
+            asd.Engine.Terminate();
+        }
+    }
+}
---
+++ cube_exp/cube_exp/Scene/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cube_exp.Scene
+{
+    class Game:asd.Scene
+    {
+
+        protected override void OnRegistered()
+        {
+
+        }
+
+        protected override void OnUpdating()
+        {
+
+        }
+    }
+}
