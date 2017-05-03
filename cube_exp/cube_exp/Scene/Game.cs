dc99b8670804ac6ab6e5d79792f4309a6b7626bf
wisteria <wisteria0x0f@users.noreply.github.com>
2017-05-03 20:27:56 +0900

    マップデータ読み込みを実装

--- cube_exp/cube_exp/Scene/Game.cs
+++ cube_exp/cube_exp/Scene/Game.cs
@@ -12,16 +12,20 @@ namespace cube_exp.Scene
         asd.CameraObject3D Camera;
         BoxObject Character;
 
-        int[,,] FieldData;
-
         protected override void OnRegistered()
         {
             Layer = new asd.Layer3D();
             AddLayer(Layer);
 
+            var testMap = new MapRawData("MapTest.txt");
+            foreach (var o in testMap)
+            {
+                Layer.AddObject(o);
+            }
+
             Camera = new asd.CameraObject3D()
             {
-                Position = new asd.Vector3DF(-5, 5, 10),
+                Position = new asd.Vector3DF(testMap.SizeX, 5, testMap.SizeZ),
                 Focus = new asd.Vector3DF(10.5f, 0, 10.5f),
                 FieldOfView = 70.0f,
                 ZNear = 1.0f,
@@ -36,36 +40,6 @@ namespace cube_exp.Scene
             };
             Layer.AddObject(light1);
 
-            FieldData = new int[20, 20, 20];
-            for (int i = 0; i < 20; i++)
-            {
-                for (int j = 0; j < 20; j++)
-                {
-                    for (int k = 0; k < 20; k++)
-                    {
-                        FieldData[i, j, k] = j == 0 ? (i + k) % 2 + 1 : 0;
-                    }
-                }
-            }
-
-            for (int i = 0; i < 20; i++)
-            {
-                for (int j = 0; j < 20; j++)
-                {
-                    for (int k = 0; k < 20; k++)
-                    {
-                        if (FieldData[i, j, k] == 1)
-                        {
-                            var b = new BoxObject()
-                            {
-                                Position = new asd.Vector3DF(i, j, k),
-                                IsDrawn = FieldData[i, j, k] == 1,
-                            };
-                            Layer.AddObject(b);
-                        }
-                    }
-                }
-            }
 
 
             Character = new Player()
---
+++ cube_exp/cube_exp/Scene/Map.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cube_exp.Scene
+{
+    class MapRawData
+    {
+        public int SizeX { get; private set; }
+        public int SizeY { get; private set; }
+        public int SizeZ { get; private set; }
+
+        public int[][][] Data;
+        public MapRawData(string Name)
+        {
+            var file = asd.Engine.File.CreateStaticFile(Name);
+            var raw = Encoding.UTF8.GetString(file.Buffer)
+                .Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x)).ToArray();
+
+            SizeX = raw[0];
+            SizeY = raw[1];
+            SizeZ = raw[2];
+
+            Data = new int[SizeY][][];
+            int counter = 3;
+            for (int y = SizeY - 1; y >= 0; y--)
+            {
+                Data[y] = new int[SizeX][];
+                for (int x = 0; x < SizeX; x++)
+                {
+                    Data[y][x] = new int[SizeZ];
+                    for (int z = 0; z < SizeZ; z++)
+                    {
+                        Data[y][x][z] = raw[counter++];
+                    }
+                }
+            }
+        }
+
+        public IEnumerator<BoxObject> GetEnumerator()
+        {
+            for (int x = 0; x < SizeX; x++)
+            {
+                for (int y = 0; y < SizeY; y++)
+                {
+                    for (int z = 0; z < SizeZ; z++)
+                    {
+                        if (Data[y][x][z] == 1)
+                        {
+                            var obj = new BoxObject()
+                            {
+                                Position = new asd.Vector3DF(x, y, z),
+
+                            };
+                            yield return obj;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
